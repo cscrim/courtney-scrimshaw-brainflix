a74959468f4e8f15b70fe50908a73f04a5033f4c
Courtney S <courtneyscrimshaw@gmail.com>
2024-10-25 19:47:07 -0400

    add comments input, avatar, and button

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,11 +1,25 @@
+import userAvatar from '../../assets/images/Mohan-muruge.jpg';
 import "./Comments.scss";
 
 function Comments({ selected }) {
   return (
     <div className="comments">
+      <div className="comments__count">
+        3 comments (update to be dynamic)
+      </div>
       <div className="comments__input-container">
-        <div className="comments__avatar"></div>
-        <div className="comments__input"></div>
+        <div className="comments__avatar">
+          <img src={userAvatar} alt="user avatar" />
+        </div>
+        <div className="comments__input">
+          <p>
+            JOIN THE CONVERSATION
+          </p>
+          <form className="comments__form">
+            <input type="text" placeholder="Add a new comment" />
+            <button type="submit">COMMENT</button>
+          </form>
+        </div>
       </div>
 
       <div className="comments__rendered-container">
